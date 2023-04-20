58a58fe3aa4144e9dc776980ecc0835b9c3344be
Michał Przybyłowski <myss1990@gmail.com>
2023-04-20 16:46:57 +0200

    Add project files.

---
+++ 20. Zadanie iterowanie po danych wejściowych wersja druga/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace Tablice
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int? FirstuserInput = null;
+            int TotalAmount = 0;
+            int PreviousUserInput = 0;
+            int HighestNumber = 0;
+            while (FirstuserInput != 0)
+            {
+                string StringUserInput = Console.ReadLine();
+                int UserInput = int.Parse(StringUserInput);
+                PreviousUserInput = TotalAmount;
+                TotalAmount = UserInput + PreviousUserInput;
+                PreviousUserInput = TotalAmount;
+                
+                if (UserInput > HighestNumber)
+                {
+                    HighestNumber = UserInput;
+                }
+               FirstuserInput = UserInput;
+
+            }
+            Console.WriteLine(TotalAmount);
+            Console.WriteLine("HighestNumber = " + HighestNumber);
+
+        }
+    }
+}
